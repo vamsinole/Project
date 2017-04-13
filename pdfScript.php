391c5247e315893ba9922b6183934866f1dd6b0a
vamsinole <vamsikrishnaenjapuri@gmail.com>
2017-04-14 03:30:04 +0530

    ui fixes

--- pdfScript.php
+++ pdfScript.php
@@ -4,16 +4,19 @@ require 'pdfcrowd.php';
 try
 {
     // create an API client instance
+    $pageWidth = 1050;
     $client = new Pdfcrowd("vamsinole", "5c014eeebd6ff3fabd732a6d851f8549");
-
+    $pdf = $client->setPageWidth($pageWidth);
+    $pdf = $client->setPageMargins(10, 10, 10, 10);
     // convert a web page and store the generated PDF into a $pdf variable
-    $pdf = $client->convertURI('http://mynewsite.local/');
+    $pdf = $client->convertURI('https://vamsinole.github.io/Project/');
+    // $pdf = $client->setPageBackgroundColor('00FF00');
 
     // set HTTP response headers
     header("Content-Type: application/pdf");
     header("Cache-Control: max-age=0");
     header("Accept-Ranges: none");
-    header("Content-Disposition: attachment; filename=\"output.pdf\"");
+    header("Content-Disposition: attachment; filename=\"solution.pdf\"");
 
     // send the generated PDF
     echo $pdf;
